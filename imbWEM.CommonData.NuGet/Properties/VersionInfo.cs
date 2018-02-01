c48d9b57d199560cc8813af145440b0e20499f4e
hardyVeles <hardy@veles.rs>
2018-02-01 05:35:17 +0100

    setup

--- imbWEM.CommonData.NuGet/Properties/VersionInfo.cs
+++ imbWEM.CommonData.NuGet/Properties/VersionInfo.cs
@@ -7,6 +7,6 @@ using System.Reflection;
 [assembly: AssemblyFileVersion("1.0.1.0")]
 [assembly: AssemblyInformationalVersion("1.0.1.0")]
 #else
-[assembly: AssemblyFileVersion("1.0.2.14")]
-[assembly: AssemblyInformationalVersion("1.0.2.14")]
+[assembly: AssemblyFileVersion("0.1.2.16")]
+[assembly: AssemblyInformationalVersion("0.1.2.16")]
 #endif
